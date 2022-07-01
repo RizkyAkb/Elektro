239d27ada4827831fb7aae141963d7a2a4de558f
RizkyAkb <rawp58@gmail.com>
2022-07-01 21:08:41 +0700

    final

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*contact*/
+const card = document.querySelector('.map-card');
+const cardBody = card.querySelector('.card-body')
+
+card.addEventListener('click', () => {
+    cardBody.classList.toggle('closed')
+});
+/*---*/
+
---
+++ js/vehicle.js
@@ -0,0 +1,85 @@
+/*model x*/
+$(function() {
+    $('#modalx_black').click(function() {
+        $("#pmodalx").attr('src', "image/modelx/modelx black.png");
+        return false;
+    });
+});
+$(function() {
+    $('#modalx_white').click(function() {
+        $("#pmodalx").attr('src', "image/modelx/modelx_white.png");
+        return false;
+    });
+});
+$(function() {
+    $('#modalx_blue').click(function() {
+        $("#pmodalx").attr('src', "image/modelx/modelxblue.png");
+        return false;
+    });
+});
+/*model 3*/
+$(function() {
+    $('#modal3_black').click(function() {
+        $("#pmodal3").attr('src', "image/model3/model3 black.png");
+        return false;
+    });
+});
+$(function() {
+    $('#modal3_white').click(function() {
+        $("#pmodal3").attr('src', "image/model3/model3 white.png");
+        return false;
+    });
+});$(function() {
+    $('#modal3_red').click(function() {
+        $("#pmodal3").attr('src', "image/model3/model3 red.png");
+        return false;
+    });
+});
+/*model y*/
+$(function() {
+    $('#modaly_red').click(function() {
+        $("#pmodaly").attr('src', "image/modely/modely.png");
+        return false;
+    });
+});
+$(function() {
+    $('#modaly_white').click(function() {
+        $("#pmodaly").attr('src', "image/modely/modely white.png");
+        return false;
+    });
+});$(function() {
+    $('#modaly_blue').click(function() {
+        $("#pmodaly").attr('src', "image/modely/modely blue.png");
+        return false;
+    });
+});
+/*model s*/
+$(function() {
+    $('#modals_black').click(function() {
+        $("#pmodals").attr('src', "image/models/models black.png");
+        return false;
+    });
+});
+$(function() {
+    $('#modals_blue').click(function() {
+        $("#pmodals").attr('src', "image/models/models blue.png");
+        return false;
+    });
+});$(function() {
+    $('#modals_red').click(function() {
+        $("#pmodals").attr('src', "image/models/models red.png");
+        return false;
+    });
+});
+/*cybertruck*/
+$(function() {
+    $('#modalc_1').click(function() {
+        $("#pmodalc").attr('src', "image/cybertruck/cybertruck_1.png");
+        return false;
+    });
+});$(function() {
+    $('#modalc_2').click(function() {
+        $("#pmodalc").attr('src', "image/cybertruck/cybertruck_2.png");
+        return false;
+    });
+});
